51afd304fb2580f4e547630ac7b545c5e9dba6a7
XushnayevAsadbek <xushnaevasadbek@gmail.com>
2023-09-12 21:39:18 +0500

    addstudent form commit

--- src/component/addsudent/addStudent.jsx
+++ src/component/addsudent/addStudent.jsx
@@ -15,6 +15,47 @@ export const AddStudent = () =>{
             </h2>
 
             <button className="btn btn-susces" onClick={onBack}>onBack</button>
+
+            <div>
+            <form className="form" >
+                <label htmlFor="task_title" className="w-50 d-block mx-auto">
+                    <p>Enter task name</p>
+                    <input type="text"
+                        id="task_title"
+                        
+                    />
+                </label>
+                <label htmlFor="task_email" >
+                    <p>Enter task email</p>
+                    <input type="email"
+                        required
+                        id="task_email"
+                       
+                        
+                    />
+                </label>
+                <label htmlFor="task_phone">
+                    <p className="text-primary fw-bold text-uppercase">Enter task phone</p>
+                    <input type="tel"
+                        required
+                        id="task_phone"
+                        className="form-control p-3 w-100"
+                        
+                    />
+                </label>
+
+                <label htmlFor="task_date">
+                    <p className="text-primary fw-bold text-uppercase">Enter task date</p>
+                    <input type="date"
+                        required
+                        id="task_date"
+                        
+                       
+                    />
+                </label>
+                <button >Add new task  </button>
+            </form>
+            </div>
         </div>
     )
 }
