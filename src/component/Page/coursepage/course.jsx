029132d2ec681f9118e429d9a3f5d514c60b658a
asadbek755 <xushnaevasadbek@gmail.com>
2024-07-08 17:21:40 +0500

    courseList page commit

--- src/component/Page/coursepage/course.jsx
+++ src/component/Page/coursepage/course.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom"
-
+import "./course.css"
 export const CoursePage = () => {
     const navigate =useNavigate();
     const onBack = () =>{
@@ -17,6 +17,42 @@ export const CoursePage = () => {
       <button className="back--btn" onClick={onBack}>onBack</button>
        <button className="add--btn"> ADD NEW Course</button>
       </div>
+       </div>
+       <div className="">
+       <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Phone</th>
+        <th>Enrol Number</th>
+        <th>Date of admission</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>John</td>
+        <td>john@gmail.com</td>
+        <td>+998</td>
+        <td>New York</td>
+        <td>22.03.2021</td>
+      </tr>
+      <tr>
+        <td>Jane</td>
+        <td>jane@gmail.com</td>
+        <td>+998</td>
+        <td>York</td>
+        <td>22.03.2021</td>
+      </tr>
+      <tr>
+        <td>Mike</td>
+        <td>mike@gmail.com</td>
+        <td>+998</td>
+        <td>York</td>
+        <td>22.03.2021</td>
+      </tr>
+    </tbody>
+  </table>
        </div>
        </div>   
     )
